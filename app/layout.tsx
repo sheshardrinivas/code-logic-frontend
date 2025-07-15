c3d8e20f44dd4ccd4d0aba0a74c1db51894cc881
sheshadrinivas <127858531+sheshardrinivas@users.noreply.github.com>
2025-07-15 18:52:35 +0530

    update fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Source_Code_Pro } from "next/font/google";
+import { Geist, Geist_Mono, Source_Code_Pro,Noto_Sans } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+
+const notoSans= Noto_Sans({
+  variable: "--font-noto-sans",
   subsets: ["latin"],
 });
 
@@ -29,7 +35,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable}  ${sourceCodePro.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable}  ${sourceCodePro.variable} ${notoSans.variable} antialiased`}
       >
         {children}
       </body>
